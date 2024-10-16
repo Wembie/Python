996196f90abcf90dcf8f238ee50f72d39368d985
Juan Acosta <73087520+Wembie@users.noreply.github.com>
2024-10-16 15:16:57 -0500

    Add files via upload

---
+++ python-testing-pytest/src/main.py
@@ -0,0 +1,13 @@
+def sum(x, y):
+    return x + y
+
+
+def is_greater_than(number_1, number_2):
+    return number_1 > number_2
+
+
+def login(username, password):
+    if ((username == "Wembie") and (password == "123456")):
+        return True
+    else:
+        return False
